3651cc0e199261acea7df4967a204533ad18ab67
Aakash-Pandit <aakashpandit366@gmail.com>
2026-05-09 12:58:16 +0530

    refactor(tools): update tools package

--- sage/tools/__init__.py
+++ sage/tools/__init__.py
@@ -1,9 +1,9 @@
 # Import all tool modules so their @REGISTRY.tool decorators fire on first import
 from sage.tools import (  # noqa: F401
     file_tools,
-    shell_tools,
-    search_tools,
     git_tools,
     retrieval_tools,
+    search_tools,
+    shell_tools,
 )
 from sage.tools.registry import REGISTRY  # noqa: F401
